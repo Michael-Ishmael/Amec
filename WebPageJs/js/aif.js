c00e764d9adb6123c432927719ec88a69c5484a3
Michael Ishmael <michaeli@perfectinfo.com>
2017-04-28 16:46:33 +0100

    Knockout implementation 1

--- WebPageJs/js/aif.js
+++ WebPageJs/js/aif.js
@@ -1,44 +1,74 @@
+function WorkflowStep(step, layout) {
 
-function WorkflowStep(title, index){
+    var self = this;
+    self.title = step.title;
+    self.index = step.index;
+    self.row = step.row;
+    self.colspan = step.colspan;
+    self.color = step.color;
+    self.template = step.isSubmit ? 'submit' : 'standard';
 
-  this.title = title;
-  this.index = index;
+    self.columnClass = ko.computed(function(){
+        if(self.colspan === 2) return "col-md-8";
+        return "col-md-4"
+    }, this);
+
+    self.columnClass = ko.computed(function(){
+        if(self.colspan === 2) return "col-md-8";
+        return "col-md-4"
+    }, this);
+
+    self.getColorClass = function(prefixes){
+        if(!$.isArray(prefixes)) return prefixes + "-" + self.color + " ";
+        var classes = '';
+        for (var i = 0; i < prefixes.length; i++) {
+            var prefix = prefixes[i];
+            classes += prefix + "-" + self.color + " ";
+        }
+        return classes.trim();
+    }
+
+    self.handleClick = function(){
+        if(layout && layout.stepClicked && typeof layout.stepClicked === "function"){
+            layout.stepClicked(self);
+        }
+    }
 
 }
 
-function WorkflowLayout(steps){
+function WorkflowLayout(steps) {
+    var self = this;
 
-  this.steps = steps.forEach(function(s){ return new WorkflowStep(step.title, step.index)});
+    self.showEdit = null; // ko.observable(true);
+    self.stepClicked = function(step){
+        self.showEdit(true);
+        // Handle step
+    };
 
+    self.steps = steps.forEach(function (s) {
+        return new WorkflowStep(s, self)
+    });
+
+    function getRowArray(steps){
+        var rowObj = steps.reduce(function(grps, s){
+            (grps[s["row"]] = grps[s["row"]] || []).push(s);
+            return grps;
+        }, {});
+
+        var rowCount = Math.max.apply(Math, steps.map(function(s){return s.row;}));
+        var rows = [];
+
+        for (var i = 0; i < rowCount; i++) {
+            var row = rowObj[i + 1];
+            rows.push(row.map(function (s) {
+                return new WorkflowStep(s);
+            }));
+        }
+        return rows;
+    }
+
+    var rows = getRowArray(steps);
+    self.rows = ko.observableArray(rows);
 }
 
-var steps = [
-  {
-    title: 'Objectives',
-    index: 1
-  },
-  {
-    title: 'Inputs',
-    index: 2
-  },
-  {
-    title: 'Inputs',
-    index: 3
-  },
-  {
-    title: 'Inputs',
-    index: 4
-  },
-  {
-    title: 'Inputs',
-    index: 5
-  },
-  {
-    title: 'Inputs',
-    index: 6
-  },
-  {
-    title: 'Inputs',
-    index: 7
-  }
-];
+ko.applyBindings(new WorkflowLayout(steps));
---
+++ WebPageJs/js/aif_data.js
@@ -0,0 +1,64 @@
+/**
+ * Created by michaeli on 28/04/2017.
+ */
+
+var steps = [
+    {
+        title: 'Objectives',
+        index: 1,
+        row: 1,
+        colspan: 1,
+        color: "red"
+    },
+    {
+        title: 'Inputs',
+        index: 2,
+        row: 1,
+        colspan: 1,
+        color: "yellow"
+
+    },
+    {
+        title: 'Activity',
+        index: 3,
+        row: 1,
+        colspan: 1,
+        color: "green"
+    },
+    {
+        title: 'Outputs',
+        index: 4,
+        row: 2,
+        colspan: 1,
+        color: "light_blue"
+    },
+    {
+        title: 'Out-takes',
+        index: 5,
+        row: 2,
+        colspan: 1,
+        color: "dark_blue"
+    },
+    {
+        title: 'Outcomes',
+        index: 6,
+        row: 2,
+        colspan: 1,
+        color: "dark_blue"
+    },
+    {
+        title: 'Impact',
+        index: 7,
+        row: 3,
+        colspan: 2,
+        color: "purple"
+    },
+    {
+        title: 'SUBMIT',
+        index: 8,
+        row: 3,
+        colspan: 1,
+        color: "black",
+        isSubmit: true
+    }
+];
